f8ec689416c31a162245d25ea0b22cadaed2696c
MARIA S LAPTOP <MARIA S LAPTOP@MARIA>
2022-10-11 16:01:26 +0300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Goldan_Maria_Valentina_lab1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
